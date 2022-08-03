5a0583c6ff18e8c387ea356972f895a58b375973
kimrojas <kurt.irvin.rojas@gmail.com>
2022-08-03 16:44:53 +0900

    MongoDB initial setup

--- flask/01-todo-mongodb/flaskMongoDB/application/__init__.py
+++ flask/01-todo-mongodb/flaskMongoDB/application/__init__.py
@@ -1,5 +1,14 @@
 from flask import Flask
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = 'e5dff1c481632e45eab9354a6a161c356373f27e'
+app.config["MONGO_URI"] = "mongodb+srv://user:userpassword@cluster0.wbgv4ax.mongodb.net/?retryWrites=true&w=majority"
+
+# Setup MongoDB
+mongodb_client = PyMongo(app)
+db = mongodb_client.db
 
 from application import routes
+
+

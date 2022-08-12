389887fb6f9dfdc51696b3451fd050eb3c6ef4d8
kimrojas <kurt.irvin.rojas@gmail.com>
2022-08-12 19:55:56 +0900

    working todo flask-mongo interaction

--- flask/01-todo-mongodb/flaskMongoDB/application/__init__.py
+++ flask/01-todo-mongodb/flaskMongoDB/application/__init__.py
@@ -7,12 +7,12 @@ dns.resolver.default_resolver.nameservers=['8.8.8.8']
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = 'e5dff1c481632e45eab9354a6a161c356373f27e'
-app.config["MONGO_URI"] = "mongodb+srv://admin:pass1234@cluster0.wbgv4ax.mongodb.net/?retryWrites=true&w=majority"
+app.config["MONGO_URI"] = "mongodb+srv://admin:pass1234@cluster0.wbgv4ax.mongodb.net/myfirstdatabase?retryWrites=true&w=majority"
 
 # Setup MongoDB
 mongodb_client = PyMongo(app)
 db = mongodb_client.db
-
+print(db)
 from application import routes
 
 
--- flask/01-todo-mongodb/flaskMongoDB/application/routes.py
+++ flask/01-todo-mongodb/flaskMongoDB/application/routes.py
@@ -1,12 +1,37 @@
 from application import app
-from flask import render_template
+from application import db
+from flask import render_template, redirect, flash, request
 from .forms  import TodoForm
+from datetime import datetime
+import pytz
 
 @app.route("/")
-def index():
-    return render_template("view_todos.html", title="Layout Page")
+def get_todos():
+    todos = []
+    for todo in db.todo_flask.find().sort("date_created", -1):
+        todo["_id"] = str(todo["_id"])
+        todo["date_created"] = todo["date_created"].strftime("%b %d %Y %H:%M:%S")
+        todos.append(todo)
+    return render_template("view_todos.html", title="Layout Page", todos=todos)
 
-@app.route("/add_todo")
+@app.route("/add_todo", methods=["POST", "GET"])
 def add_todo():
-    form = TodoForm()
+    if request.method == "POST":
+        form = TodoForm(request.form)
+        todo_name = form.name.data
+        todo_description = form.description.data
+        completed = form.completed.data
+        
+        db.todo_flask.insert_one({
+            "name": todo_name,
+            "description": todo_description,
+            "completed": completed,
+            "date_created": datetime.now()
+        })
+        
+        flash("Todo successfully added", "success")
+        return redirect("/")
+        
+    else:
+        form = TodoForm()
     return render_template("add_todo.html", form=form)
